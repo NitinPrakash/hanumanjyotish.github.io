81a45a4786442afaaea87dc217ebbbaede7f75b4
Mona Verma <nitin247@outlook.com>
2019-11-14 15:02:09 +0530

    Code Push

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# hanumanjyotish.io
+# Hanuman Jyotish
+This is git to Janmakshar Jyotish's Hanuman Jyotish Web App
---
+++ free-astrology-games-for-sites.php
@@ -0,0 +1,148 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+<title>Free astrology games for websites,Free jyotish games for websites,Janmakshar,Janmakshar Jyotish,Janmakshar Free,Free Astropathy,Hanuman Jyotish,Jyotish Games,Astrology Games,Vedic Indian Astrology,Best Indian Astrology,Top Jyotish,Best Astrology,Best Astrologer,Top Astrologer,Top Jyotish in Lucknow, India</title>
+<meta name="google-site-verification" content="MDhRadBdCMfZ6QVUATE75fC81jT_3o9rXep3wP4LThw" />
+<meta http-equiv="Page-Enter" content="blendTrans(Duration=0.01)">
+<meta http-equiv="Page-Exit" content="blendTrans(Duration=0.01)">
+<meta name="author" content="Nitin Prakash" />
+<meta name="subject" content="Astrology, Astropathy Guidance And Consulting" />
+<meta name="Description" content="Please contact us to discuss whether we can Help you !!!" />
+<meta name="Classification" content="Jyotish,Astrology,Astropathy,Clairvoyance,Fortune Feller" />
+<meta name="Keywords" content="Hanuman Jyotish,Free Jyotish Games,Free Astrology Games" />
+<Meta name = "keyphrase" content = "janmakshar free,janmakshar,janmakshar jyotish,Best Jyotish Site,Best Astrology Site,Vedic Astrology,Indian Astrology,Free Astrology Consultation,Jyotish India,Astrology India,Jyotish Indian,Astrologer India,Top vedic astrology,top jyotish India,top Astrologer India,Free Astropathy,Free Astropathy India,Hanuman Jyotish,Jyotish Games,Astrology Games">
+<meta name="Language" content="English" />
+<meta HTTP-EQUIV="PRAGMA" content="NO-CACHE" />
+<meta name="Copyright" content="Copyright 2011,All Rights Reserved" />
+<meta name="Revisit-After" content="1 days" />
+<meta name="distribution" content="Global" />
+<META NAME="robots" CONTENT="index, follow">
+<META NAME="GOOGLEBOT" CONTENT="INDEX, FOLLOW">
+<meta name="city" content="Lucknow" />
+<meta name="country" content="India" />
+<meta name="MSSmartTagsPreventParsing" content="TRUE" />
+
+<link href="css/example.css" rel="stylesheet" type="text/css" media="screen" />
+<link href="css/facebook.alert.css" rel="stylesheet" type="text/css" media="screen" />
+<link type='text/css' href='css/contact.css' rel='stylesheet' media='screen' />
+<link type='text/css' href="css/jquery.bookmark.css" rel='stylesheet' media='screen' />
+<script charset="utf-8" type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.6/jquery.min.js"></script>
+<script charset="utf-8" type="text/javascript" src="js/jquery.badBrowser.js"></script>
+<script type="text/javascript" language="javascript">
+ $(document).ready(function(){   
+   $("a.game").click(function(event){
+     var pageURL = $(this).attr("href");
+     PopupCenter(pageURL, '',900,600)
+     return false;
+   });
+ });
+ 
+function PopupCenter(pageURL, title,w,h) {
+var left = (screen.width/2)-(w/2);
+var top = (screen.height/2)-(h/2);
+var targetWin = window.open (pageURL, title, 'toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=no, resizable=no, copyhistory=no, width='+w+', height='+h+', top='+top+', left='+left);
+}
+</script>
+
+<style type="text/css">
+  
+  table.inner { background: #feffff; /* Old browsers */
+				background: -moz-linear-gradient(top, #feffff 0%, #ddf1f9 35%, #a0d8ef 100%); /* FF3.6+ */
+				background: -webkit-gradient(linear, left top, left bottom, color-stop(0%,#feffff), color-stop(35%,#ddf1f9), color-stop(100%,#a0d8ef)); /* Chrome,Safari4+ */
+				background: -webkit-linear-gradient(top, #feffff 0%,#ddf1f9 35%,#a0d8ef 100%); /* Chrome10+,Safari5.1+ */
+				background: -o-linear-gradient(top, #feffff 0%,#ddf1f9 35%,#a0d8ef 100%); /* Opera11.10+ */
+				background: -ms-linear-gradient(top, #feffff 0%,#ddf1f9 35%,#a0d8ef 100%); /* IE10+ */
+				filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#feffff', endColorstr='#a0d8ef',GradientType=0 ); /* IE6-9 */
+				background: linear-gradient(top, #feffff 0%,#ddf1f9 35%,#a0d8ef 100%); /* W3C */
+				-moz-border-radius:10px; -webkit-border-radius:10px; -khtml-border-radius:10px; border-radius:10px;
+
+ }
+ 
+ table.inner td{ vertical-align:middle; text-align:center; -moz-border-radius:3px; -webkit-border-radius:3px; -khtml-border-radius:3px; border-radius:3px; }
+ table.inner a{ font-weight:normal; font-size:14px; color:#990000; text-decoration:none; font-weight:bold; }
+ table.inner a:hover{ font-weight:normal; font-size:14px; color:#00CC00; font-weight:bold; }
+ table.inner th { border:none;}
+  
+</style>
+</head>
+<body>
+<script charset="utf-8" type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-17282201-6']);
+  _gaq.push(['_setDomainName', '.janmakshar.co.in']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
+<script type="text/javascript">
+window.google_analytics_uacct = "UA-17282201-6";
+</script>
+<div id="main">
+  <table width="100%">
+    <tr>
+	  <td align="center">
+	    <table class="inner" width="400" border="1" cellpadding="5" cellspacing="5">
+		    <tr><th colspan="3"><a href="http://www.janmakshar.co.in/Hanuman-Jyotish.htm" target="_blank">Hanuman Jyotish</a> By <a href="http://www.janmakshar.co.in" target="_blank">www.janmakshar.co.in</a></th></tr>
+			<tr>
+			  <td><a class="game" href="http://hanuman-jyotish.janmakshar.co.in/ManKaamPariksha.html">मन:काम परीक्षा </a></td>
+			  <td><a class="game" href="http://hanuman-jyotish.janmakshar.co.in/MilanPariksha.html">मिलन परीक्षा </a></td>
+			  <td><a class="game" href="http://hanuman-jyotish.janmakshar.co.in/VidyaPariksha.html">विद्या परीक्षा </a></td>
+			</tr>
+			<tr>
+			  <td><a class="game" href="http://hanuman-jyotish.janmakshar.co.in/Vivah-Pariksha.html"> विवाह परीक्षा </a></td>
+			  <td><a class="game" href="http://hanuman-jyotish.janmakshar.co.in/Vyapar-Pariksha.html">व्यापार परीक्षा</a></td>
+			  <td><a class="game" href="http://hanuman-jyotish.janmakshar.co.in/Rog-Pariksha.html">रोग परीक्षा </a></td>
+			</tr>
+			<tr>
+			  <td><a class="game" href="http://hanuman-jyotish.janmakshar.co.in/Garbh-Pariksha.html">गर्भ परीक्षा</a></td>
+			  <td><a class="game" href="http://hanuman-jyotish.janmakshar.co.in/Santaan-Pariksha.html">संतान परीक्षा</a></td>
+			  <td><a class="game" href="http://hanuman-jyotish.janmakshar.co.in/Yachna-Pariksha.html">याचना परीक्षा</a></td>
+			</tr>
+			<tr>
+			  <td><a class="game" href="http://hanuman-jyotish.janmakshar.co.in/Gaman-Pariksha.html">गमन परीक्षा</a></td>
+			  <td><a class="game" href="http://hanuman-jyotish.janmakshar.co.in/Agaman-Pariksha.html">आगमन परीक्षा</a></td>
+			  <td><a class="game" href="http://hanuman-jyotish.janmakshar.co.in/Sahayata-Pariksha.html">सहायता परीक्षा</a></td>
+		    </tr>
+			<tr>
+			  <td><a class="game" href="http://hanuman-jyotish.janmakshar.co.in/Dhan-Chinta-Pariksha.html">धन चिंता परीक्षा</a></td>
+			  <td><a class="game" href="http://hanuman-jyotish.janmakshar.co.in/PraptiPariksha.html">प्राप्ति परीक्षा</a></td>
+			  <td><a class="game" href="http://hanuman-jyotish.janmakshar.co.in/GoodhShatroPariksha.html">गूढ़शत्रो:परीक्षा</a></td>
+		    </tr>
+			<tr>
+			  <td><a class="game" href="http://hanuman-jyotish.janmakshar.co.in/Devesht-Pariksha.html">देवेष्ट परीक्षा</a></td>
+			  <td><a class="game" href="http://hanuman-jyotish.janmakshar.co.in/Ganga-Prapti-Pariksha.html">गंगा प्राप्ति परीक्षा</a></td>
+			  <td><a class="game" href="http://hanuman-jyotish.janmakshar.co.in/Mrityu-Chinta-Pariksha.html">मृत्यु चिंता परीक्षा</a></td>
+		    </tr>
+			<tr>
+			  <td><a class="game" href="http://hanuman-jyotish.janmakshar.co.in/VasniRupan-Pariksha.html">वास्निरूपन परीक्षा</a></td>
+			  <td><a class="game" href="http://hanuman-jyotish.janmakshar.co.in/Sang-Pariksha.html">संग परीक्षा</a></td>
+			  <td>&nbsp;</td>
+		    </tr>
+            <tr>
+			    <td colspan="3">
+				    <div style="text-align:center">
+					<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<ins class="adsbygoogle"
+     style="display:inline-block;width:300px;height:250px"
+     data-ad-client="ca-pub-5118816121775309"
+     data-ad-slot="5613431886"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script></div>
+
+				</td>
+			</tr>						  
+  		</table>
+	  </td>
+	</tr>  
+  </table>
+</div>
+</body>
+</html>
